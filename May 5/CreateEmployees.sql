f2a3a167a21775dea28231a2ea761633d3feb2c6
Vinay Kumar G <kumargv896@gmail.com>
2025-05-05 11:52:01 +0530

    employees

---
+++ May 5/CreateEmployees.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Employees (
+	EmpID INT PRIMARY KEY,
+	Name Varchar(50),
+	Department Varchar(50),
+	Salary DECIMAL(10,2),
+	JoinDate DATE
+);
---
+++ May 5/InsertEmployees.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employees (EmpID, Name, Department, Salary, JoinDate)
+VALUES
+(1,'John','HR',50000,'2021-06-01'),
+(2,'Priya','Finance',60000,'2020-09-15'),
+(3,'Amit','IT',75000,'2019-01-10'),
+(4,'Sara','IT',72000,'2023-02-25');
