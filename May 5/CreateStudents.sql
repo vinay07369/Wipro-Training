ad52277c6547da5f20f4d68489d5b12737c55c86
Vinay Kumar G <kumargv896@gmail.com>
2025-05-05 11:37:09 +0530

    students

---
+++ May 5/CreateStudents.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Students (
+	StudentID INT PRIMARY KEY,
+	FirstName Varchar(50),
+	Marks INT,
+	DOB Date
+);
---
+++ May 5/InsertStudents.sql
@@ -0,0 +1,6 @@
+INSERT INTO Students (StudentID, FirstName, Marks, DOB)
+VALUES
+(1,'Amit',92,'2003-05-10'),
+(2,'Neha',78,'2002-11-15'),
+(3,'Rahul',61,'2004-01-20'),
+(4,'Sneha',49,'2003-09-01');
