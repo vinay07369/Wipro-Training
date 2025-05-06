d193f2bbce5e25c9f59e469c31b42acf94168036
Vinay Kumar G <kumargv896@gmail.com>
2025-05-06 15:42:27 +0530

    countwords

--- May 6/Assignments/CountWords.cs
+++ May 6/Assignments/CountWords.cs
@@ -2,12 +2,24 @@ using System;
 
 public class Words
 {
+    static int count(string s)
+    {
+        int count = 0;
+
+        string[] a = s.Split(' ');
+
+        for(int i=0;i<a.Length;i++)
+        {
+            count++;
+        }
+
+        return count;
+
+    }
     public static void Main(string[] args)
     {
         string s = "Learning c# is Fun";
         
-        string[] a = s.Split(' ');
-        
-        Console.WriteLine("Total words:"+a.Length);
+        Console.WriteLine("Total words:"+ count(s));
     }
 }
