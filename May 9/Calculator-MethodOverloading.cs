6b38c0a01623b860e33783131c66c8499e75ea05
Vinay Kumar G <kumargv896@gmail.com>
2025-05-09 15:06:04 +0530

    method overloading

--- May 9/Calculator-MethodOverloading.cs
+++ May 9/Calculator-MethodOverloading.cs
@@ -20,5 +20,33 @@ namespace MethodOverloading
         public int Add(int a, int b, int c) { 
             return a + b + c;
         }
+
+        //Subtraction overloading
+
+        public int Sub(int a, int b) {
+            return a - b;
+        }
+
+        public double Sub(double a, double b) {
+            return a - b;
+        }
+
+        public double Sub(double a, int b, int c) {
+            return a - b - c;
+        }
+
+        //Multiplication overloading
+
+        public int Mul(int a, int b) {
+            return a * b;
+        }
+
+        public double Mul(double a, int b) { 
+            return (double)a * b;
+        }
+
+        public double Mul(int a, double b, int c) {
+            return a * b * c;
+        }
     }
 }
--- May 9/MainCalculator.cs
+++ May 9/MainCalculator.cs
@@ -22,6 +22,30 @@ namespace MethodOverloading
             Console.WriteLine($"The Addition of two decimal numbers:{c2}");
             Console.WriteLine($"The Addition of three numbers:{c3}");
 
+            Console.WriteLine("--------------------------------");
+
+            int s1 = c.Sub(9, 4);
+
+            double s2 = c.Sub(8.5, 4.2);
+
+            double s3 = c.Sub(2.3, 3, 4);
+
+            Console.WriteLine($"The Subtraction of two numbers:{s1}");
+            Console.WriteLine($"The Subtraction of two decimal numbers:{s2}");
+            Console.WriteLine($"The Subtraction of three numbers:{s3}");
+
+            Console.WriteLine("--------------------------------");
+
+            int m1 = c.Mul(9, 4);
+
+            double m2 = c.Mul(8.5, 2);
+
+            double m3 = c.Mul(2, 3.2, 4);
+
+            Console.WriteLine($"The Multiplication of two numbers:{m1}");
+            Console.WriteLine($"The Multiplication of two decimal numbers:{m2}");
+            Console.WriteLine($"The Multiplication of three numbers:{m3}");
+
         }
     }
 }
