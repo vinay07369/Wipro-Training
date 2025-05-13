3cf6909eb5fee3db6ba2e64abfda635328c176e6
Vinay Kumar G <kumargv896@gmail.com>
2025-05-13 12:22:02 +0530

    delegates

---
+++ May 13/SomeOperation_Delegates2.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_practice
+{
+    internal class Delegates
+    {
+
+        public delegate int SomeOperation(int x, int y);
+        class Program
+        {
+            static int Sum(int x, int y)
+            { 
+                return x + y;
+            }
+            static void Main(string[] args)
+            {
+                Func<int, int, int> add = Sum; // using Func keyword, it accepts two parameters as an inputs, and the third one is output based on the return type...
+
+                int result = add(5, 2);
+
+                Console.WriteLine(result);
+            }
+        }      
+    }
+}
