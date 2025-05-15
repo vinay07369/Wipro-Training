5e4e0512b4dab935e0aba9b28a01a022168a8e6c
Vinay Kumar G <kumargv896@gmail.com>
2025-05-15 15:13:17 +0530

    entityFramework

---
+++ May 15/StudentModel.cs
@@ -0,0 +1,10 @@
+namespace MyWebAPP.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+    }
+}
