dca0fa648f5ce14e0e9e2478fa7a651302b0f512
Vinay Kumar G <kumargv896@gmail.com>
2025-05-17 09:49:15 +0530

    27377

---
+++ May 15/EmployeeWebApp/Data/EmployeeWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeWebApp.Models;
+
+namespace EmployeeWebApp.Data
+{
+    public class EmployeeWebAppContext : DbContext
+    {
+        public EmployeeWebAppContext (DbContextOptions<EmployeeWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeWebApp.Models.Employee> Employee { get; set; } = default!;
+    }
+}
