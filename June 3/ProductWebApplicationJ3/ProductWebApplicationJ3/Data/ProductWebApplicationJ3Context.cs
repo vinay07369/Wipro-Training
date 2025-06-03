8eb9e1c81abee48b5dcf0c15cd7ad47608bcec7b
Vinay Kumar G <kumargv896@gmail.com>
2025-06-03 12:01:18 +0530

    productWebAPI

---
+++ June 3/ProductWebApplicationJ3/ProductWebApplicationJ3/Data/ProductWebApplicationJ3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductWebApplicationJ3.Models;
+
+namespace ProductWebApplicationJ3.Data
+{
+    public class ProductWebApplicationJ3Context : DbContext
+    {
+        public ProductWebApplicationJ3Context (DbContextOptions<ProductWebApplicationJ3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductWebApplicationJ3.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ June 3/ProductWebApplicationJ3/ProductWebApplicationJ3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProductWebApplicationJ3.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ProductWebApplicationJ3Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductWebApplicationJ3Context") ?? throw new InvalidOperationException("Connection string 'ProductWebApplicationJ3Context' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
